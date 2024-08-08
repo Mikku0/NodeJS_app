b955174977f8a13e6748e3eca7579a81abd88ebc
Mikku0 <mikolaj080803@gmail.com>
2024-08-09 00:00:05 +0200

    init2

--- server.js
+++ server.js
@@ -1,17 +1,44 @@
-const add = (a, b) => a+b;
+// const http = require('http');
 
-var five = 5
-var four = 4
-const substract = (a, b) => a-b;
+// const hostname = '127.0.0.1';
+// const port = 3000;
 
-console.log('kdlsakl');
-const multiply = (a, b) => a.repeat(b);
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello, World!\n');
+// });
 
-console.log(multiply('abc ', 3));
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
 
-var newString = multiply('aaa', 3)
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-console.log(newString);
+const hostname = '127.0.0.1';
+const port = 3000;
 
+const server = http.createServer((req, res) => {
+  // Ustal ścieżkę do pliku HTML
+  const filePath = path.join(__dirname, 'index.html');
 
+  // Sprawdź, czy żądany plik istnieje
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.statusCode = 500;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('Internal Server Error\n');
+      return;
+    }
+    // Ustaw odpowiedni nagłówek Content-Type
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end(data);
+  });
+});
 
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
