23e213c92a5ddefbe3ffa019e93eed3e7aaa0c64
Mikku0 <mikolaj080803@gmail.com>
2024-08-08 23:12:57 +0200

    init

---
+++ server.js
@@ -0,0 +1,17 @@
+const add = (a, b) => a+b;
+
+var five = 5
+var four = 4
+const substract = (a, b) => a-b;
+
+console.log('kdlsakl');
+const multiply = (a, b) => a.repeat(b);
+
+console.log(multiply('abc ', 3));
+
+var newString = multiply('aaa', 3)
+
+console.log(newString);
+
+
+
